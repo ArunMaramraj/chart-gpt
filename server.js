754c1646baf81627084ed1ccf94ca00fd4d6797a
Arun Maramraj <imarun500@gmail.com>
2025-02-03 01:31:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const session = require('express-session'); // Import express-session
 
 const app = express();
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 
 // Middleware for parsing JSON data
 app.use(bodyParser.json());
